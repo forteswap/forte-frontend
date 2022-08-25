82989a16634f7d380413e6c510f49f5171616d9a
khivedoteth <khivedoteth>
2022-08-25 12:56:51 -0400

    remove string check

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -36,8 +36,7 @@ export const fetchPoolData = (pairs) => {
                 const isStable = await poolContract.stable();
                 const token1 = systemTokens.where('address',poolTokens[0]).first();
                 const token2 = systemTokens.where('address',poolTokens[1]).first();
-                const token1EconomicValue = token1.address == '0x826551890Dc65655a0Aceca109aB11AbDbD7a07B' ? 0.30 : 0 
-                const token2EconomicValue = token2.address = '0xdE59F060D7ee2b612E7360E6C1B97c4d8289Ca2e' ? 1 : 0
+
 
                 const share = LpBalanceFinal/LpTotalSupplyFinal;
                 const tokenNames = poolSymbol.split('-')[1].split('/');
@@ -60,8 +59,10 @@ export const fetchPoolData = (pairs) => {
                 userPoolAmount[poolTokens[1]] = getNumberValue(share * poolAmountToken2,decimal2);
                 totalPoolAmount[poolTokens[1]] = getNumberValue(poolAmountToken2,decimal2);
                 sharePer[poolTokens[1]] = 0;
-
-     const tvlForPool = (poolAmountToken1 * token1EconomicValue) + (poolAmountToken2 * token2EconomicValue);
+                console.log(token1.address);   
+                const token1EconomicValue = token1.address == 0x826551890Dc65655a0Aceca109aB11AbDbD7a07B ? 0.30 : 0 
+                const token2EconomicValue = token2.address == 0xdE59F060D7ee2b612E7360E6C1B97c4d8289Ca2e ? 1 : 0
+                const tvlForPool = (poolAmountToken1 * token1EconomicValue) + (poolAmountToken2 * token2EconomicValue);
                 const singleLp = {
                     pairName: poolSymbol,
                     isStable: isStable,
