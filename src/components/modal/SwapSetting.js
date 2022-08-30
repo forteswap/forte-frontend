19233fc8048ad13322e8f9d691282982cb8c7429
khivedoteth <khivedoteth>
2022-08-30 18:51:33 -0400

    remove settings options, group token together

--- src/components/modal/SwapSetting.js
+++ src/components/modal/SwapSetting.js
@@ -32,43 +32,6 @@ const SwapSetting = (props) => {
                             <Input className="form-control"/>
                         </Col>
                     </Row>
-                    <Row className="mt-4">
-                        <Col sm={12}>
-                           <p> Transaction Deadline? <span className="text-secondary">Best time is between 1-5 min</span></p>
-                        </Col>
-                        <Col sm={4}>
-                            <Input className="form-control" value={30}/>
-                        </Col>
-                        <Col sm={3} className="ps-0">
-                            <p className="ps-0">Minutes</p>
-                        </Col>
-                    </Row>
-                    <Row className="mt-4">
-                        <Col sm={12}>
-                            <p> Interface Settings</p>
-                            <hr/>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col sm="8">
-                            <p>Auto Router API </p>
-                        </Col>
-                        <Col sm="4">
-                            <div className="form-check form-switch row">
-                                <input className="form-check-input cursor-pointer" type="checkbox" role="switch" />
-                            </div>
-                        </Col>
-                    </Row>
-                    <Row className="mt-3">
-                        <Col sm="8">
-                            <p>Expert Mode </p>
-                        </Col>
-                        <Col sm="4">
-                            <div className="form-check form-switch row">
-                                <input className="form-check-input cursor-pointer" type="checkbox" role="switch" />
-                            </div>
-                        </Col>
-                    </Row>
                 </ModalBody>
                 <ModalFooter className="with-bg full-btn">
                     <Button color="none" className="btn-starch btn btn-lg">
