fdb5d18d40722a5aa26fe2c5bdc5aa0f76ffdfc0
khivedoteth <khivedoteth>
2022-08-31 18:29:38 -0400

    remove google form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,15 +5,6 @@ const Footer = () => {
     return (
         <>
             <Container className="mt-5">
-                <Row>
-                    <Col sm={12} className="text-center">
-                        <p className="text-white">
-                            Help improve ForteSwap by <a className="text-white fw-bold"
-                                                         href="https://forms.gle/exMxUju3iRGyLXLV9" target="_blank"
-                                                         rel="external">reporting bugs here.</a>
-                        </p>
-                    </Col>
-                </Row>
             </Container>
         </>
     );
