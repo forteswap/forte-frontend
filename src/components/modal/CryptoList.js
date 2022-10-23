361a33b709a1227fed9e430e8647c2a4cb111747
0xfoobar <83792390+0xfoobar@users.noreply.github.com>
2022-10-23 16:55:48 -0400

    add ENCANTO(#8)
    Add ENCANTO

--- src/components/modal/CryptoList.js
+++ src/components/modal/CryptoList.js
@@ -46,7 +46,6 @@ const CryptoListModal = (props) => {
             name: symbol,
             address: newAddress,
             decimal: 18,
-            stable: false,
         };
         onValueUpdate(cryptoCoin);
     }
