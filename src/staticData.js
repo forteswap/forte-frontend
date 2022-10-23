02bec2ed032b8b567b008957b7fc663233a561f6
0xfoobar <0xfoobar1@gmail.com>
2022-10-23 16:54:13 -0400

    add encanto, remove stable attribute from known coins

--- src/staticData.js
+++ src/staticData.js
@@ -14,7 +14,6 @@ export const cryptoCoinsEnum = {
         name: "CANTO",
         address: "0x826551890Dc65655a0Aceca109aB11AbDbD7a07B",
         decimal: 18,
-        stable:false,
     },
     wcanto: {
         icon: "canto.svg",
@@ -22,7 +21,6 @@ export const cryptoCoinsEnum = {
         name: "WCANTO",
         address: "0x826551890Dc65655a0Aceca109aB11AbDbD7a07B",
         decimal: 18,
-        stable:false,
     },
     eth: {
         icon: "eth.png",
@@ -30,7 +28,6 @@ export const cryptoCoinsEnum = {
         name: "ETH",
         address: "0x5FD55A1B9FC24967C4dB09C513C3BA0DFa7FF687",
         decimal: 18,
-        stable:false,
     },
     atom: {
         icon: "atom.png",
@@ -38,7 +35,6 @@ export const cryptoCoinsEnum = {
         name: "ATOM",
         address: "0xecEEEfCEE421D8062EF8d6b4D814efe4dc898265",
         decimal: 6,
-        stable:false,
     },
     cinu: {
         icon: "cinu.png",
@@ -46,7 +42,6 @@ export const cryptoCoinsEnum = {
         name: "cINU",
         address: "0x7264610A66EcA758A8ce95CF11Ff5741E1fd0455",
         decimal: 18,
-        stable:false,
     },
     ceth: {
         icon: "ceth.svg",
@@ -54,7 +49,6 @@ export const cryptoCoinsEnum = {
         name: "CETH",
         address: "0x830b9849E7D79B92408a86A557e7baAACBeC6030",
         decimal: 18,
-        stable:false,
     },
     cusdc: {
         icon: "cusdc.svg",
@@ -62,7 +56,6 @@ export const cryptoCoinsEnum = {
         name: "CUSDC",
         address: "0xdE59F060D7ee2b612E7360E6C1B97c4d8289Ca2e",
         decimal: 6,
-        stable:true,
     },
     cusdt: {
         icon: "cusdt.svg",
@@ -70,7 +63,6 @@ export const cryptoCoinsEnum = {
         name: "CUSDT",
         address: "0x6b46ba92d7e94FfA658698764f5b8dfD537315A9",
         decimal: 6,
-        stable:true,
 
     },
     note: {
@@ -79,7 +71,6 @@ export const cryptoCoinsEnum = {
         name: "NOTE",
         address: "0x4e71A2E537B7f9D9413D3991D37958c0b5e1e503",
         decimal: 18,
-        stable:false,
     },
     usdc: {
         icon: 'usdc.png',
@@ -87,7 +78,6 @@ export const cryptoCoinsEnum = {
         name: "USDC",
         address: "0x80b5a32E4F032B2a058b4F29EC95EEfEEB87aDcd",
         decimal: 6,
-        stable:true,
     },
     usdt: {
         icon: "usdt.png",
@@ -95,7 +85,13 @@ export const cryptoCoinsEnum = {
         name: "USDT",
         address: "0xd567B3d7B8FE3C79a1AD8dA978812cfC4Fa05e75",
         decimal: 6,
-        stable:true,
+    },
+    encanto: {
+        icon: "encanto.png",
+        title: "Encanto",
+        name: "ENC",
+        address: "0x373A8cD983948c5c20759dFae763D3bb7790F383",
+        decimal: 18,
     }
 };
 
