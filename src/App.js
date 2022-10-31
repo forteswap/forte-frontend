37cc9a331eb8e1f4470ccf50c65912ca79757cce
yolodevz <104152852+zebrahacker0x@users.noreply.github.com>
2022-10-31 15:52:53 +0000

    build: implement toastify container

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import "./App.scss";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import React from 'react'
 import {Route, Routes} from 'react-router-dom'
-import Main from "./layouts/Main";
-import Pools from "./views/pools/Index";
-import CreatePool from "./views/pools/Create";
-import AddLiquidity from "./views/pools/liquidity/Add";
-import RemoveLiquidity from "./views/pools/liquidity/Remove";
-import Swap from "./views/swap/Index";
+import Main from "./layouts/Main.js";
+import Pools from "./views/pools/Index.js";
+import CreatePool from "./views/pools/Create.js";
+import AddLiquidity from "./views/pools/liquidity/Add.js";
+import RemoveLiquidity from "./views/pools/liquidity/Remove.js";
+import Swap from "./views/swap/Index.js";
 
 const App = () => {
     return (
         <Main>
+            <ToastContainer />
             <Routes>
                 <Route exact path="/pool" element={<Pools/>}/>
                 <Route exact path={"/pool/create"} element={<CreatePool/>}/>
