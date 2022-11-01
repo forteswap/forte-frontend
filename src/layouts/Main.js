7f5ed913a1635905a3bd3161185473e7477f303f
zebrahacker0x <104152852+zebrahacker0x@users.noreply.github.com>
2022-11-01 15:14:13 +0000

    feature: dirty add the notice component (#17)
    * yarn add react-cookie
    
    * create Notice.js component
    
    * implement Notice component alongside the cookie provider
    
    * fix an empty page when ethereum doesn't exist on the window object
    
    * add Notice container gap
    
    * adjust notice styles

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import Navigation from "../components/Navigation";
+import { CookiesProvider } from 'react-cookie';
 import GlobalModalProvider from "../components/modal/GlobalModal";
 import Footer from "../components/Footer";
+import Notice from "../components/notice/Notice";
 
 const Main = ({children}) => {
     return (
         <React.Fragment>
-            <GlobalModalProvider>
-                <main className="mb-5">
-                    <Navigation/>
-                    {children}
-                    <Footer/>
-                </main>
-            </GlobalModalProvider>
+            <CookiesProvider>
+                <GlobalModalProvider>
+                    <main className="mb-5">
+                        <Notice/>
+                        <Navigation/>
+                        {children}
+                        <Footer/>
+                    </main>
+                </GlobalModalProvider>
+            </CookiesProvider>
         </React.Fragment>
     );
 };
