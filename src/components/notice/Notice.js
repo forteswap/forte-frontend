cd61a66e3b648770734227d348c5a18e6b78f8cc
yolodevz <104152852+zebrahacker0x@users.noreply.github.com>
2022-11-01 15:05:38 +0000

    implement Notice component alongside the cookie provider

--- src/components/notice/Notice.js
+++ src/components/notice/Notice.js
@@ -1,12 +1,12 @@
 import React, {useCallback} from 'react';
 import { useCookies } from 'react-cookie';
-import {X as CloseIcon} from 'react-feather';
 
 const Notice = () => {
     let provider = window.ethereum;
     const [cookie, setCookie] = useCookies(['hasUserOptedOut']);
 
-    const {hasUserOptedOut} = cookie
+    const SLINGSHOT_RPC_URL = 'https://canto.slingshot.finance/';
+    const {hasUserOptedOut} = cookie;
 
     const handleClose = useCallback(() => {
         setCookie('hasUserOptedOut', true, { path: '/' });
@@ -17,25 +17,23 @@ const Notice = () => {
             method: 'wallet_addEthereumChain',
             params: [{
                 chainId: '0x1e14',
-                rpcUrls: ['https://canto.slingshot.finance/'],
+                rpcUrls: [SLINGSHOT_RPC_URL],
                 chainName: 'CANTO',
                 nativeCurrency: {
                     name: 'CANTO',
-                    symbol: 'CANTO', // 2-6 characters long
+                    symbol: 'CANTO',
                     decimals: 18,
                 },
             }]
         });
     }, [provider])
 
-    console.log('hasUserOptedOut', hasUserOptedOut)
-
     return (
         !hasUserOptedOut && <div style={{backgroundColor: '#ebbe67', color: '#272179', padding: '1rem 2rem', textAlign: 'center', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
             <div>
 
             We recommend connecting using the <span style={{fontWeight: 'bold', cursor: 'pointer'}}
-              onClick={handleAddSlingshotRPC} title={'https://canto.slingshot.finance/'}>Slingshot RPC</span> for the best experience.
+              onClick={handleAddSlingshotRPC} title={SLINGSHOT_RPC_URL}>Slingshot RPC</span> for the best experience.
             </div>
             <span style={{cursor: 'pointer'}}>
                 <span onClick={handleClose} style={{backgroundColor: 'rgb(255 230 123)', padding: '0.5rem 1rem', borderRadius: '1rem', fontWeight: 'bold'}}>I understand</span>
