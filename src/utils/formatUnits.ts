89f3ee316a01a4f1c0880bb360f78ca50bce6755
zebrahacker0x <104152852+zebrahacker0x@users.noreply.github.com>
2022-11-07 18:54:59 +0000

    feature: implement safe numerical input (#28)
    * feature: implement safeguard regex
    
    * refactor: change util files to typescript
    
    * refactor: implement numerical input
    
    * cleanup: remove console log
    
    * feature: implement input debounce

--- src/utils/formatUnits.ts
+++ src/utils/formatUnits.ts
@@ -1,13 +1,15 @@
-// @todo implement with typescript later
 import { formatUnits as formatUnitsEthers, formatEther } from '@ethersproject/units';
+import { BigNumber } from '@ethersproject/bignumber';
 
-export const parseUnitsForView = (bigNumberAmount) => {
+// @todo implement safety guards
+export const parseUnitsForView = (bigNumberAmount: BigNumber): string => {
     // cuts down to maximum 3 decimal points
     const remainder = bigNumberAmount.mod(1e14);
 
     return formatEther(bigNumberAmount.sub(remainder));
 }
 
-export const formatUnits = (bigNumberAmount, decimals, shouldParseUnitsForView = false) => {
+// @todo implement safety guards
+export const formatUnits = (bigNumberAmount: BigNumber, decimals: BigNumber, shouldParseUnitsForView = false): string => {
     return shouldParseUnitsForView ? parseUnitsForView(bigNumberAmount) : formatUnitsEthers(bigNumberAmount, decimals);
 }
