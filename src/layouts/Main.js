cffc399327aa25faf2c56226bfac698bda32ee4e
zebrahacker0x <104152852+zebrahacker0x@users.noreply.github.com>
2022-11-12 15:30:20 +0000

    refactor: create and apply page content container (#31)

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -4,16 +4,19 @@ import { CookiesProvider } from 'react-cookie';
 import GlobalModalProvider from "../components/modal/GlobalModal";
 import Footer from "../components/Footer";
 import Notice from "../components/notice/Notice";
+import Container from '../components/container';
 
 const Main = ({children}) => {
     return (
         <React.Fragment>
             <CookiesProvider>
                 <GlobalModalProvider>
-                    <main className="mb-5">
+                    <main className="tw-w-full tw-h-full tw-flex tw-flex-col">
                         <Notice/>
                         <Navigation/>
-                        {children}
+                        <Container>
+                            {children}
+                        </Container>
                         <Footer/>
                     </main>
                 </GlobalModalProvider>
